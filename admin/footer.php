bc33e0d61b3da93aa01143ce1a792be91d3113fa
zainaftab44 <zainaftab44@gmail.com>
2018-05-11 23:40:06 +0500

    complete project

--- admin/footer.php
+++ admin/footer.php
@@ -1,70 +1,8 @@
 </div>
-        <!-- jQuery -->
-        <!-- <script src="../vendor/jquery/jquery.min.js"></script> -->
+<script src="../dist/js/sb-admin-2.js"></script>
+<script src="../js/jquery.tablesorter.min.js"></script>
 
-        <!-- Bootstrap Core JavaScript -->
-        <!-- <script src="../vendor/bootstrap/js/bootstrap.min.js"></script> -->
-
-        <!-- Metis Menu Plugin JavaScript -->
-        <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-
-        <!-- Custom Theme JavaScript -->
-        <script src="../dist/js/sb-admin-2.js"></script>
-        <script src="../js/jquery.tablesorter.min.js"></script>
-        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
-
-        <script>
-            $(function () {
-
-                $('#calendar').fullCalendar({
-                    defaultView: 'month',
-                    events: 'https://fullcalendar.io/demo-events.json'
-                });
-
-            });
-        </script> -->
-
-         <script>
-        //     $(document).ready(function () {
-        //         $('#datePicker')
-        //                 .datepicker({
-        //                     format: 'mm/dd/yyyy'
-        //                 })
-        //                 .on('changeDate', function (e) {
-        //                     // Revalidate the date field
-        //                     $('#eventForm').formValidation('revalidateField', 'date');
-        //                 });
-
-        //         $('#eventForm').formValidation({
-        //             framework: 'bootstrap',
-        //             icon: {
-        //                 valid: 'glyphicon glyphicon-ok',
-        //                 invalid: 'glyphicon glyphicon-remove',
-        //                 validating: 'glyphicon glyphicon-refresh'
-        //             },
-        //             fields: {
-        //                 name: {
-        //                     validators: {
-        //                         notEmpty: {
-        //                             message: 'The name is required'
-        //                         }
-        //                     }
-        //                 },
-        //                 date: {
-        //                     validators: {
-        //                         notEmpty: {
-        //                             message: 'The date is required'
-        //                         },
-        //                         date: {
-        //                             format: 'MM/DD/YYYY',
-        //                             message: 'The date is not a valid'
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //         });
-        //     });
-        </script>
-    </body>
+</script>
+</body>
 
 </html>
--- admin/header.php
+++ admin/header.php
@@ -1,63 +1,43 @@
 <!DOCTYPE html>
 <?php include "dbconn.php";
 session_start();
-if(!isset($_SESSION['usr'])) {
+if (!isset($_SESSION['usr'])) {
     header("Location:login.php");
 }?>
 <html lang="en">
 
-    <head>
+<head>
 
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        <title>Sales management system</title>
-        <!-- Bootstrap Core CSS -->
-        <!-- <link href="./css/bootstrap.min.css" rel="stylesheet"> -->
-        <meta charset='UTF-8'><meta name="robots" content="noindex">
+    <title>Sales management system</title>
+    <meta charset='UTF-8'>
+    <meta name="robots" content="noindex">
 
-    <!-- Bootstrap Core CSS -->
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- MetisMenu CSS -->
-    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
     <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <!-- <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"> -->
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-        <!-- Custom Fonts -->
-        <!-- <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"> -->
-        <!--        <link href="../css/calender.min.css" rel="stylesheet" type="text/css">-->
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css'><link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $("#myTable").tablesorter();
-    });
-</script>
-<style>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css'>
+    <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("#myTable").tablesorter();
+        });
+    </script>
+    <style>
         th.headerSortUp {
             background-image: url('../images/asc.gif');
             /* background-color: #3399FF;  */
             background-repeat: no-repeat;
             background-position: center right;
         }
-
+        
         th.headerSortDown {
             background-image: url('../images/desc.gif');
             /* background-color: #3399FF;  */
@@ -65,56 +45,51 @@ if(!isset($_SESSION['usr'])) {
             background-position: center right;
         }
     </style>
-      </head>
+</head>
 
-    <body>
-        <div id="wrapper">
+<body>
+    <div id="wrapper">
 
-            <!-- Navigation -->
-            <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.html">Sales Panel</a>
+                <a class="navbar-brand" href="index.html">Sales Panel</a>
+            </div>
+            <!-- /.navbar-header -->
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li>
+                            <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="addbill.php"><i class="fa fa-bar-chart-o fa-fw"></i>Add Order</a>
+                        </li>
+                        <li>
+                            <a href="billslist.php"><i class="fa fa-table fa-fw"></i>List Orders</a>
+                        </li>
+                        <li>
+                            <a href="listitems.php"><i class="fa fa-edit fa-fw">List Stock</i> </a>
+                        </li>
+                        <li>
+                            <a href="additem.php"><i class="fa fa-wrench fa-fw"></i>Add Stock</a>
+                        </li>
+                        <li>
+                            <a href="addproduct.php"><i class="fa fa-sitemap fa-fw"></i> Add Product</a>
+                        </li>
+                        <li>
+                            <a href="listproducts.php"><i class="fa fa-sitemap fa-fw"></i> List Products</a>
+                        </li>
+                        <li>
+                            <a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
                 </div>
-                <!-- /.navbar-header -->
-                <div class="navbar-default sidebar" role="navigation">
-                    <div class="sidebar-nav navbar-collapse">
-                        <ul class="nav" id="side-menu">
-                            <li>
-                                <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-                            </li>
-                            <li>
-                                <a href="addbill.php"><i class="fa fa-bar-chart-o fa-fw"></i>Add Order</a>
-                            </li>
-                            <li>
-                                <a href="billslist.php"><i class="fa fa-table fa-fw"></i>List Orders</a>
-                            </li>
-                            <li>
-                                <a href="listitems.php"><i class="fa fa-edit fa-fw">List Stock</i> </a>
-                            </li>
-                            <li>
-                                <a href="additem.php"><i class="fa fa-wrench fa-fw"></i>Add Stock</a>
-                            </li>
-                            <li>
-                                <a href="addproduct.php"><i class="fa fa-sitemap fa-fw"></i> Add Product</a>
-                            </li>
-                            <li>
-                                <a href="listproducts.php"><i class="fa fa-sitemap fa-fw"></i> List Products</a>
-                            </li>
-                            <li>
-                                <a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                            </li>
 
-
-                            <!-- <li>
-                                <a href="salesreports.php"><i class="fa fa-sitemap fa-fw"></i> Reports</a>
-                            </li> -->
-                        </ul>
-                    </div>
-
-            </nav>
+        </nav>
